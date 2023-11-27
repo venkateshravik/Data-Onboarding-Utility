a1efd68d77638264846e0cbc4c09133194893fbc
subhrajyoti sau <subhrajyotisau@google.com>
2023-11-27 15:31:42 +0530

    added user-data-mapping&target-table-ingestion

--- backend/dot/bqconnector/models.py
+++ backend/dot/bqconnector/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -8,6 +8,7 @@ from django.db import models
 class JobIdStore(models.Model):
     name = models.CharField(max_length=300)
     created_date = models.DateTimeField(auto_now=True)
+    user         = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -17,6 +18,7 @@ class BigqueryInfo(models.Model):
     dataset_name = models.CharField(max_length=1024)
     source_table_id = models.CharField(max_length=1024)
     target_table_id = models.CharField(max_length=1024)
+    user            = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.bigquery_file_name
